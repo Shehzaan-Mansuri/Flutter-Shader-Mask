a2c1157ed4ea30b0f70090349fdbb9fbc1d2ed4d
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-10-26 20:25:08 +0530

    Shader Mask

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Shader Mask"),
+      ),
+      body: ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return RadialGradient(
+            center: Alignment.topLeft,
+            radius: 1.0,
+            colors: <Color>[Colors.yellow, Colors.deepOrange.shade900],
+            tileMode: TileMode.mirror,
+          ).createShader(bounds);
+        },
+        child: const Text('I’m burning the memories'),
+      ),
+    );
+  }
+}
